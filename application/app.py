df0cf7f8013241e5e82cc4c278945e6f5ed49f28
koretaka-ai <93076958+koretaka-ai@users.noreply.github.com>
2022-10-15 20:57:07 +0900

    Update app.py

--- application/app.py
+++ application/app.py
@@ -12,11 +12,11 @@ model_dir = "../translate/results/SEED33/checkpoints"
 model_name = "checkpoint_best.pt"
 spm_en = "../translate/pre-trained/enja_spm_models/spm.en.nopretok.model"
 
-if '{model_dir}/dict.en.txt' == False:
-    shutil.copy('{preprocess_dir}/dict.en.txt', '{model_dir}')
+if os.path.isfile(f'{model_dir}/dict.en.txt') == False:
+    shutil.copy(f'{preprocess_dir}/dict.en.txt', f'{model_dir}')
 
-if '{model_dir}/dict.ja.txt' == False:
-    shutil.copy('{preprocess_dir}/dict.ja.txt', '{model_dir}')
+if os.path.isfile(f'{model_dir}/dict.ja.txt') == False:
+    shutil.copy(f'{preprocess_dir}/dict.ja.txt', f'{model_dir}')
 
 model = TransformerModel.from_pretrained(model_dir, model_name)
 spm_model = spm.SentencePieceProcessor(model_file=spm_en)
@@ -46,4 +46,4 @@ def trans():
     return render_template('index.html', ja=ja, en=en)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
