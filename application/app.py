f2ca60741ef2df7dbc166b027207544bc68338d8
koretaka-ai <93076958+koretaka-ai@users.noreply.github.com>
2022-10-15 20:48:10 +0900

    Update app.py

--- application/app.py
+++ application/app.py
@@ -1,18 +1,25 @@
+import os
+import shutil
 from flask import Flask, render_template, request
-import os, subprocess, sys
+from fairseq.models.transformer import TransformerModel
+import sentencepiece as spm
 
 app = Flask(__name__)
 
 # --- PATH settings ---
-model = "../translate/results/SEED33/checkpoints/checkpoint_best.pt"
+preprocess_dir = "../translate/datasets/small_parallel_enja/sentencepiece/fairseq-preprocess"
+model_dir = "../translate/results/SEED33/checkpoints"
+model_name = "checkpoint_best.pt"
 spm_en = "../translate/pre-trained/enja_spm_models/spm.en.nopretok.model"
-spm_ja = "../translate/pre-trained/enja_spm_models/spm.ja.nopretok.model"
 
-datasets = "./datasets/"
-input = datasets + "test.en"
-input_spm = datasets + "sentencepiece/spm.en"
-output = "./result/result"
-dest_dir = "../translate/datasets/small_parallel_enja/sentencepiece/fairseq-preprocess"
+if '{model_dir}/dict.en.txt' == False:
+    shutil.copy('{preprocess_dir}/dict.en.txt', '{model_dir}')
+
+if '{model_dir}/dict.ja.txt' == False:
+    shutil.copy('{preprocess_dir}/dict.ja.txt', '{model_dir}')
+
+model = TransformerModel.from_pretrained(model_dir, model_name)
+spm_model = spm.SentencePieceProcessor(model_file=spm_en)
 
 @app.route('/')
 def index():
@@ -20,46 +27,23 @@ def index():
 
 @app.route('/', methods=['post'])
 def trans():
+    
     en = request.form['en']
     ja = en
     answer = ''
     if not en:
         answer = ''
         return render_template('index.html', ja=answer, en=en)
-    # 翻訳したい文の書き込み
-    with open(input, "w") as f:
-        f.write(en)
-    
+
     # spmの前処理
-    spm_preprocess = "spm_encode --model={} --output_format=piece < {} > {}".format(spm_en, input, input_spm)
-    subprocess.run(spm_preprocess, shell=True)
-    
-    # データの翻訳
-    fairseq_interactive = "CUDA_VISIBLE_DEVICES=0,1,2,3 /home/koretaka/anaconda3/envs/trans-en-ja/bin/fairseq-interactive {} \
-    --input {} \
-    --path {} \
-    --batch-size 64 --buffer-size 1024 \
-    --nbest 1 --beam 5 \
-    > {}.txt".format(dest_dir, input_spm, model, output)
-    subprocess.run(fairseq_interactive, shell=True)
-    
-    # データの抽出
-    result_grep = "grep '^H-' {}.txt | sort -V | cut -f3 > {}.ja".format(output, output)
-    subprocess.run(result_grep, shell=True)
+    spm_en = spm_model.encode(en, out_type=str)
+    spm_en = ' '.join(spm_en)
 
-    detokenize = "cat {}.ja | sed 's/<<unk>>/<unk>/g' | sed 's/ //g' | sed 's/▁/ /g' | sed 's/^[ \t]*//g' | sed 's/ //g' > {}.detok.ja".format(output, output)
-    subprocess.run(detokenize, shell=True)
+    # 翻訳
+    spm_ja = model.translate(spm_en)
+    ja = spm_ja.replace('▁', '').replace(' ', '')
 
-    with open(output + ".detok.ja", "r") as file:
-        lines = file.readlines()
-        s_lines = [line.strip() for line in lines]
-        for line in s_lines:
-            answer = line
-        
-    return render_template('index.html', ja=answer, en=en)
+    return render_template('index.html', ja=ja, en=en)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# uWSGIはデフォルトでapplicationを探すためapplicationのaliasを書く必要があるらしい
-# application = app
